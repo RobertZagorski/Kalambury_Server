c14d5fde1a2d7a43ac59313b0dbdd00b8a8b87fc
Robert <robertzagorsky@gmail.com>
2013-05-21 13:00:16 +0200

    version 1.03: added changing of client status

--- client.cpp
+++ client.cpp
@@ -3,10 +3,24 @@
 
 client::client() {}
 
+client::client(const client &clnt)
+{
+    this->clientStatus = clnt.clientStatus;
+    this->c_socket = clnt.c_socket;
+    this->evbase = clnt.evbase;
+    this->in_buffer = clnt.in_buffer;
+    this->out_buffer = clnt.out_buffer;
+}
+
 client::~client() {}
 
 
-STATUS* client::getStatus()
+client::STATUS* client::getStatus()
 {
 	return clientStatus;
+}
+
+void client::setStatus(STATUS* status_)
+{
+	this->clientStatus = status_;
 }
--- client.h
+++ client.h
@@ -1,29 +1,35 @@
 #ifndef CLIENT_H
 #define CLIENT_H
+#include <memory>
 
-enum STATUS {DRAW,GUESS,OBSERVE};
 class client
 {
-private:
-	/*The status of a client. Indicates what rights the client has in the game*/
-	STATUS* clientStatus;
 public:	
-	/* The client's socket. */
+
+    enum STATUS {DRAW,GUESS,OBSERVE};
+	/**The client's socket. */
     int c_socket;
 
-	/* The event_base for this client. */
+	/**The event_base for this client. */
     struct event_base *evbase;
 
-    /* The bufferedevent for this client. */
+    /**The bufferedevent for this client. */
     struct bufferevent *in_buffer;
 
-    /* The output buffer for this client. */
+    /**The output buffer for this client. */
     struct evbuffer *out_buffer;
-	/*The default constructor*/
+	/**The default constructor*/
 	client();
+    /**copy constructor*/
+    client (const client &);
 	/*The default deconstructor*/
 	~client();
-	/*The method used to retrieve the status of a client*/
-	STATUS* getStatus(); 
+	/**The method used to retrieve the status of a client*/
+	STATUS* getStatus();
+    /**The method used to set the status of a client*/
+    void setStatus(STATUS*);
+private:
+	/**The status of a client. Indicates what rights the client has in the game*/
+	STATUS* clientStatus;
 };
 #endif //CLIENT_H
